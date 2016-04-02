8bf7a50ae17d1cba41e8225aa97032f950d69bfb
Mokul1158 <sarat_ch@hotmail.com>
2016-04-02 15:06:29 +0700

    Finish 2 April 16

--- app/src/main/java/appksl/mokul1158/learntraffics/MainActivity.java
+++ app/src/main/java/appksl/mokul1158/learntraffics/MainActivity.java
@@ -1,13 +1,41 @@
 package appksl.mokul1158.learntraffics;
 
+import android.media.MediaPlayer;
+import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    //  Access  Type  Name
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget คือ การผูกความสัมพันธ์ ของ Variable and Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //การเก็บเหตุการณ์ จากการคลิกปุ่ม
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.elephant);
+                mediaPlayer.start();
+
+            } // onClick
+        });
+
+
+    } // Main Method นี่คือ เมทธอด
+} // Main Class นี่คือคลาสหลัก
