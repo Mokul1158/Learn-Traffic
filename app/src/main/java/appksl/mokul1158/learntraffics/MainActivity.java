2234bd3a767b1ab24f612acbe922ab4f345c9b55
Mokul1158 <sarat_ch@hotmail.com>
2016-04-02 11:56:25 +0700

    tset

---
+++ app/src/main/java/appksl/mokul1158/learntraffics/MainActivity.java
@@ -0,0 +1,13 @@
+package appksl.mokul1158.learntraffics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
